42fbebd413c9bec7ce7cb2e85bdef1fc49b03d2f
asemFoqha <asem.sh.foqha@gmail.com>
2023-11-05 15:59:47 -0500

    mapping the groups

--- forntend/src/components/home/GroupGrid.tsx
+++ forntend/src/components/home/GroupGrid.tsx
@@ -1,10 +1,16 @@
 import { GridItem, SimpleGrid } from "@chakra-ui/react";
 import GroupCard from "./GroupCard";
+import useGroups from "../../hooks/useGroups";
 
 const GroupGrid = () => {
+  const { data, error } = useGroups();
+
+  if (error) return null;
   return (
     <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
-      <GroupCard />
+      {data?.map((group) => (
+        <GroupCard key={group._id} />
+      ))}
     </SimpleGrid>
   );
 };
